8204c4a276c2c8c36cd190216e5a28c0b6374425
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-23 16:29:15 -0600

    page appears complete, form not validating

--- source.js
+++ source.js
@@ -4,18 +4,33 @@ $(document).ready(function(){
 
   $('.parallax').parallax();
   getFoodNamePrice();
+  submitListen();
 });
 
-
+var total = 0;
 /*******************EVENT LISTENERS**********************/
 function getFoodNamePrice() {
-  var card = $('.card');
+  var card = $(".card");
 
   card.on("click", function() {
     var namePrice = this.innerText.split("\n").filter(String);
     namePrice.pop();
     updateTable(namePrice);
   });
+}
+
+// event listener for submit button
+function submitListen() {
+  $("#submitBtn").on("click", function() {
+    if (total === 0) {
+      var $toastContentF = $('<span>Please submit an order!</span>');
+      Materialize.toast($toastContentF, 5000);
+    }
+    else if (total > 0) {
+      var $toastContentP = $('<span>Your order has been submitted!</span>');
+      Materialize.toast($toastContentP, 5000);
+    }
+   });
 }
 /*******************EVENT LISTENERS**********************/
 
@@ -37,8 +52,8 @@ function updateTotals() {
   });
   subtotal = parseFloat(subtotal).toFixed(2);
   var tax = parseFloat(subtotal * 0.12).toFixed(2);
-  var total = parseFloat(subtotal + tax).toFixed(2);
-
+  total = parseFloat(subtotal) + parseFloat(tax);
+  total = total.toFixed(2);
   $('#subtotal').html(`<td> Subtotal: </td><td>$${subtotal}</td>`);
   $('#tax').html(`<td> Tax: </td><td>$${tax}</td>`);
   $('#total').html(`<td>Total: </td><td>$${total}</td>`);
@@ -47,8 +62,6 @@ function updateTotals() {
 /*******************UPDATING TABLE**********************/
 
 /*******************SUBMIT FORM**********************/
-// event listener for submit button
-// go to submit page with order invoice
 
 
 /*******************SUBMIT FORM**********************/
